94ace093e6da624437406839932a8ee5c02ee9df
Ali Piry <alipiry@disroot.org>
2018-12-04 13:57:23 +0330

    ✨ Add mui.js and pollyfills.js
    These .js files are auto-generated

--- src/pollyfills.js
+++ src/pollyfills.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 //This file includes polyfills needed by TypeScript when using es2017, es6 or any above es5
 // This file is loaded before the app. You can add your own extra polyfills to this file
-import 'core-js'
+require("core-js");
