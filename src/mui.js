f754cd78482b7ff791eb00f0eefc7b5a0b32bee7
Ali Piry <alipiry@disroot.org>
2018-12-04 14:59:54 +0330

    ✨ Update mui.ts
    Add header banner of 'MUI'

--- src/mui.js
+++ src/mui.js
@@ -1,3 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("./polyfills");
+require("./pollyfills");
+var chalk_1 = require("chalk");
+var figlet = require("figlet");
+var log = console.log;
+log(chalk_1.default.yellow(figlet.textSync('M U I', {
+    horizontalLayout: 'full',
+})));
--- src/mui.ts
+++ src/mui.ts
@@ -1,1 +1,16 @@
-import './polyfills'
+import './pollyfills'
+import chalk from 'chalk';
+import * as figlet from 'figlet';
+
+const log = console.log;
+
+log(
+  chalk.yellow(
+    figlet.textSync(
+      'M U I',
+      {
+        horizontalLayout: 'full',
+      }
+    )
+  )
+);
