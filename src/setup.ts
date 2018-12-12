e217c40b47f0e6964706228350282a2f0046606e
Ali Piry <alipiry@disroot.org>
2018-12-12 22:16:56 +0330

    ✨ Update setup func

--- src/setup.ts
+++ src/setup.ts
@@ -1,18 +1,24 @@
-import {codeConfig, colors, locations, robotsPlain, teamsPlain, wirelessProfiles} from "./values";
+import {
+  codeConfig,
+  colors,
+  locations, robots,
+  teamsPlain,
+  wirelessProfiles
+} from "./values";
 import inquirer from "inquirer";
 import fs from "fs";
 
 const questions: Array<object> = [
   { type: 'list', name: 'team', message: 'Choose your team', choices: teamsPlain, default: 'MRL-SPL (26)' },
-  { type: 'checkbox', name: 'robots', message: 'Choose your robots', choices: robotsPlain },
+  { type: 'checkbox', name: 'robots', message: 'Choose your robots', choices: robots },
   { type: 'list', name: 'color', message: 'Choose your color', choices: colors, default: 'blue' },
   { type: 'list', name: 'location', message: 'Choose your location', choices: locations, default: 'Lab' },
-  { type: 'list', name: 'wireless', message: 'Choose your wireless profile', choices: wirelessProfiles, default: 'Lab.wpa' },
+  { type: 'list', name: 'wireless', message: 'Choose your wireless profile', choices: wirelessProfiles, default: 'naoLab.wpa' },
   { type: 'list', name: 'config', message: 'Choose your code config', choices: codeConfig, default: 'Develop' },
-  { type: 'input', name: 'magicNumber', message: 'Choose your magic number:', default: 85},
+  { type: 'input', name: 'magicNumber', message: 'Choose your magic number:', default: 85 },
 ];
 
-export const prompt: any = () => {
+export const setup: any = () => {
   inquirer
     .prompt(questions)
     .then(answers =>
