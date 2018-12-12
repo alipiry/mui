ed94b968a1bee688e89d0e52e94b24be84ff4435
Ali Piry <alipiry@disroot.org>
2018-12-12 03:57:13 +0330

    ✨ Update mui.ts

--- src/mui.ts
+++ src/mui.ts
@@ -1,48 +1,7 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
-import * as figlet from 'figlet';
-import program from 'commander';
-import inquirer from 'inquirer';
-const log = console.log;
+import { banner } from "./banner";
+import { prompt } from "./prompt";
 
-const banner = () => {
-  log(
-    chalk.yellow(
-      figlet.textSync(
-        'M U I',
-        {
-          horizontalLayout: 'full',
-        }
-      )
-    )
-  );
-};
-
-const showVersion = () => {
-  program
-    .version('1.0.0', '-v, --version')
-    .parse(process.argv);
-  return true;
-};
-
-const q = () => {
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'q1',
-      message: 'Are you enjoy it?'
-    }
-  ])
-  .then(() => {
-    log(':)');
-  });
-};
-
-if(!showVersion()) {
-  banner();
-  q();
-}
 banner();
-q();
+prompt();
