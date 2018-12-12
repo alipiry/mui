975c03c47b47596042bec66c82dff6fc82b2427c
Ali Piry <alipiry@disroot.org>
2018-12-12 22:17:18 +0330

    ✨ Update mui.ts

--- src/mui.ts
+++ src/mui.ts
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 
 import { banner } from "./banner";
-import { prompt } from "./prompt";
+import { setup } from "./setup";
+import { isInRightDir } from "./rightDir";
+import chalk from "chalk";
 
-banner();
-prompt();
+if (isInRightDir) {
+  banner();
+  setup();
+} else {
+  console.log(chalk.white.bgRed.bold(
+    'You are not in Right Directory!!!\nYour working dir must have "Make" and "Config" dirs!'
+  ));
+}
