357b8d5f48e009e59031cf737fd34a7ae7c5fd2b
Ali Piry <alipiry@disroot.org>
2018-12-17 02:53:02 +0330

    ⚡️  Improve setup option

--- src/setup.ts
+++ src/setup.ts
@@ -1,15 +1,17 @@
-import {
-  codeConfig,
-  colors,
-  locations, robots,
-  teamsPlain,
-  wirelessProfiles
-} from "./values";
 import inquirer from "inquirer";
 import fs from "fs";
 
+import {
+  codeConfig,
+  colors,
+  locations,
+  robots,
+  teamsPlain,
+  wirelessProfiles
+} from "./values";
+
 const questions: Array<object> = [
-  { type: 'list', name: 'team', message: 'Choose your team', choices: teamsPlain, default: 'MRL-SPL (26)' },
+  { type: 'list', name: 'team', message: 'Choose your team', choices: teamsPlain, default: 'MRL-SPL: 26' },
   { type: 'checkbox', name: 'robots', message: 'Choose your robots', choices: robots },
   { type: 'list', name: 'color', message: 'Choose your color', choices: colors, default: 'blue' },
   { type: 'list', name: 'location', message: 'Choose your location', choices: locations, default: 'Lab' },
@@ -17,6 +19,12 @@ const questions: Array<object> = [
   { type: 'list', name: 'config', message: 'Choose your code config', choices: codeConfig, default: 'Develop' },
   { type: 'input', name: 'magicNumber', message: 'Choose your magic number:', default: 85 },
 ];
+
+const showConfig = (): void => {
+  let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+  console.log('\nThe compiled code will deploy with this config: \n');
+  console.log(config);
+};
 
 export const setup = (): void => {
   inquirer.prompt(questions).then(answers =>
@@ -28,5 +36,5 @@ export const setup = (): void => {
         '  '
       )
     )
-  );
+  ).then(() => showConfig());
 };
