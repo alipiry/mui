e1126c9d5db1849cc8c0b415bc008399614bd8fc
Ali Piry <alipiry@disroot.org>
2018-12-17 02:53:22 +0330

    ✨ Add deploy option

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,8 +1,16 @@
-// import fs from "fs";
-//
-// const showConfig = (): void => {
-//   let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-//   console.log('The compiled code will deploy with this config: \n');
-//   console.log(config);
-//   console.log('\n');
-// };
+import fs from "fs";
+// import shelljs from "shelljs";
+
+export const deploy = () => {
+  let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+  let team = config.team.split(': ')[1];
+  let robots = config.robots
+    .toString()
+    .match(/:\d*/mg)
+    .toString()
+    .split(':')
+    .join('')
+    .split(',');
+  // console.log(team);
+  // console.log(robots);
+};
